e1d66c9075404add6aa1921ecaa09dbebb62091c
MIGUEL6-BNX <luismiguel10608@gmail.com>
2025-08-03 00:04:23 -0500

    Initial project final 3.1 backend- and fronent.1.3.22 next api.1 p2

--- backend/build.sh
+++ backend/build.sh
@@ -2,10 +2,6 @@
 # exit on error
 set -o errexit
 
-echo "Initializing the database..."
-python init_db.py
+pip install -r requirements.txt
 
-echo "Database initialized. Starting Gunicorn..."
-# The port is automatically set by Render.
-# The worker-tmp-dir is a fix for some file system issues on Render.
-exec gunicorn --worker-tmp-dir /dev/shm app:app
+python init_db.py
